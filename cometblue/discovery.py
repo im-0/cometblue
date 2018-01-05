c675d02bbc245348847ef92ad3e4a3af67045cc1
Lukas Rucka <359687@mail.muni.cz>
2018-01-05 19:47:50 +0100

    Split device discovery to candidate probing + candidate discovery

--- cometblue/discovery.py
+++ cometblue/discovery.py
@@ -16,10 +16,24 @@ _SUPPORTED_DEVICES = (
 
 _log = logging.getLogger(__name__)
 
+def probe_candidate(_device):
+    name = _device.alias()
+    address = _device.mac_address
+    try:
+        with _device as device:
+            manufacturer_name = device.get_manufacturer_name().lower()
+            model_number = device.get_model_number().lower()
 
-def discover(manager, timeout=10):
-    _log.info('Starting discovery on adapter "%s" with %u seconds timeout...',
-              manager.adapter_name, timeout)
+            if (manufacturer_name, model_number) in _SUPPORTED_DEVICES:
+                return (device.mac_address, str(name))
+
+    except RuntimeError as exc:
+        _log.debug('Skipping device "%s" ("%s"), reason: %r' % (name, address, str(exc)))
+    return None
+
+
+def discover_candidates(manager, timeout=10):
+    _log.info('Probing for candidate devices...')
 
     manager.start_discovery()
     time.sleep(timeout)
@@ -27,23 +41,17 @@ def discover(manager, timeout=10):
 
     devices = manager.devices()
     _log.debug('All discovered devices: %r', [(device.mac_address, str(device.alias())) for device in devices])
+    return devices
+
+def discover(manager, timeout=10):
+    _log.info('Starting discovery on adapter "%s" with %u seconds timeout...',
+              manager.adapter_name, timeout)
+    devices = discover_candidates(manager, timeout)
 
     filtered_devices = {}
-
-    for _device in devices:
-        name = _device.alias()
-        address = _device.mac_address
-        try:
-            with _device as device:
-                manufacturer_name = device.get_manufacturer_name().lower()
-                model_number = device.get_model_number().lower()
-
-                if (manufacturer_name, model_number) in _SUPPORTED_DEVICES:
-                    filtered_devices[device.mac_address] = name
-
-        except RuntimeError as exc:
-            _log.debug('Skipping device "%s" ("%s") because of '
-                       'exception: %r' % (name, address, exc))
-
+    for device in devices:
+        device_entry = cometblue.discovery.probe_candidate(device)
+        if not device_entry is None:
+            filtered_devices.update(dict([device_entry]))
     _log.info('Discovery finished')
     return filtered_devices
